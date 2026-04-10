48d38ff633e42c80519d1e547843d0fafcf5d843
noampima <amitfinalyear@gmail.com>
2026-04-10 13:37:14 +0300

    almost done with main , need to fix cipher and decipher
    bugged cipher and decipher

--- ex1/main.c
+++ ex1/main.c
@@ -8,11 +8,53 @@
 
 // your code goes here
 
-#define MAX_ARGUMENTS 4
-#define MIN_ARGUMENT 1
+#define MAX_ARGUMENTS 5
+#define MIN_ARGUMENT 2
 #define ARGUMENT_ERROR "The program receives 1 or 4 arguments only.\n"
 #define FIRST_WORD_ERROR "The given argument is invalid.\n"
 #define TEST_ERROR "Usage: cipher test\n"
+#define FILE_ERROR "The given file is invalid.\n"
+#define BUFFER 1024
+
+int cipher_file(char input_file[], char output_file[],int k) {
+    char curr_line[BUFFER];
+    FILE *input = fopen(input_file,"r");
+    FILE *output = fopen(output_file,"w");
+
+    if (!input) {
+        printf(FILE_ERROR);
+        return EXIT_FAILURE;
+    }
+    while (fgets(curr_line, sizeof(curr_line), input)) {
+        cipher(curr_line, k);
+        fputs(curr_line, output);
+    }
+
+    fclose(input);
+    fclose(output);
+    return EXIT_SUCCESS;
+}
+
+int decipher_file(char input_file[], char output_file[],int k) {
+    char curr_line[BUFFER];
+    FILE *input = fopen(input_file,"r");
+    FILE *output = fopen(output_file,"w");
+
+    if (!input) {
+        printf(FILE_ERROR);
+        return EXIT_FAILURE;
+    }
+
+    while (fgets(curr_line, sizeof(curr_line), input)) {
+        decipher(curr_line, k);
+        fputs(curr_line, output);
+    }
+
+    fclose(input);
+    fclose(output);
+    return EXIT_SUCCESS;
+}
+
 int main(int argc, char *argv[]) {
 
     if (argc != MIN_ARGUMENT && argc != MAX_ARGUMENTS) {
@@ -22,9 +64,14 @@ int main(int argc, char *argv[]) {
 
     if (argc == MIN_ARGUMENT) {
 
-        if (strcmp(argv[0], "test") != 0) {
+        if (strcmp(argv[1], "test") != 0) {
             printf(TEST_ERROR);
             return EXIT_FAILURE;
+        }
+
+        if (strcmp(argv[1], "test") == 0) {
+
+
         }
     }
 
@@ -34,9 +81,12 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    if (strcmp(argv[1], "cipher") != 0) {
-        int k = strtol(argv[3],NULL,10);
-        
+    if (strcmp(argv[1], "cipher") == 0) {
+        int k = (int)strtol(argv[4], NULL, 10);
+        return cipher_file(argv[2], argv[3], k);
 
+    } else if (strcmp(argv[1], "decipher") == 0) {
+        int k = (int)strtol(argv[4], NULL, 10);
+        return decipher_file(argv[2], argv[3], k);
     }
 }
