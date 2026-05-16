8d7b7c080be7787b54a0c1aef2f88f963fac3464
noampima <amitfinalyear@gmail.com>
2026-05-16 15:56:47 +0300

    ex3 part a started

---
+++ ex3/tweets_generator.c
@@ -0,0 +1,6 @@
+//Don't change the macros!
+#define FILE_PATH_ERROR "Error: incorrect file path\n"
+#define NUM_ARGS_ERROR  "Usage: tweets_generator <seed> <num_tweets> "\
+                        "<file_path> [words_to_read] [seed_word]\n"
+
+#define DELIMITERS " \n\t\r"
